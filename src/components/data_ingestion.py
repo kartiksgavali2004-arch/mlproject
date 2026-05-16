742f88d6b1c4e8f8bc0f21f98310cb8ef2279a46
kartik <kartiksgavali2004@gmail.com>
2026-05-17 04:37:41 +0530

    datatransformation

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,71 @@
+import os
+import sys
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df_path = os.path.join(PROJECT_ROOT, "notebook", "data", "stud.csv")
+            df = pd.read_csv(df_path)
+            logging.info('Read the dataset as dataframe')
+
+            train_dir = os.path.join(PROJECT_ROOT, os.path.dirname(self.ingestion_config.train_data_path))
+            os.makedirs(train_dir, exist_ok=True)
+
+            raw_path = os.path.join(PROJECT_ROOT, self.ingestion_config.raw_data_path)
+            df.to_csv(raw_path, index=False, header=True)
+
+            logging.info("Train test split initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_path = os.path.join(PROJECT_ROOT, self.ingestion_config.train_data_path)
+            test_path = os.path.join(PROJECT_ROOT, self.ingestion_config.test_data_path)
+            train_set.to_csv(train_path, index=False, header=True)
+
+            test_set.to_csv(test_path, index=False, header=True)
+
+            logging.info("Ingestion of the data is completed")
+
+            return (
+                os.path.join(PROJECT_ROOT, self.ingestion_config.train_data_path),
+                os.path.join(PROJECT_ROOT, self.ingestion_config.test_data_path)
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+   
+
+
+
